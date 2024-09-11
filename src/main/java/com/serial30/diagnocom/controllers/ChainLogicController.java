9568586b9a7d4d5156d2b0ca015fcb5541083eb2
Gama19999 <gama.rios0110@gmail.com>
2024-09-11 15:12:28 -0600

    Logging
    Configuration for file logging
    Changes in main path

--- src/main/java/com/serial30/diagnocom/controllers/ChainLogicController.java
+++ src/main/java/com/serial30/diagnocom/controllers/ChainLogicController.java
@@ -5,6 +5,9 @@ import com.serial30.diagnocom.pojos.response.MessageResponse;
 import com.serial30.diagnocom.pojos.request.IncomingData;
 import com.serial30.diagnocom.services.RuleChainingService;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -12,8 +15,10 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin("*")
-@RequestMapping("chainlogic")
+@RequestMapping("${app.name}")
 public class ChainLogicController {
+
+    private static final Logger logger = LoggerFactory.getLogger(ChainLogicController.class);
 
     @Autowired
     private RuleChainingService ruleChainingService;
@@ -23,10 +28,16 @@ public class ChainLogicController {
             path = "forward",
             produces = MediaType.APPLICATION_JSON_VALUE,
             consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> doForwardChaining(@RequestBody IncomingData data) throws FactNotInferredException {
+    public ResponseEntity<?> doForwardChaining(HttpServletRequest request, @RequestBody IncomingData data) throws FactNotInferredException {
+        logRequestData(request);
         var responseData = ruleChainingService.doForwardChaining(data.getChoices());
         var messageResponse = new MessageResponse();
         messageResponse.setData(responseData);
         return ResponseEntity.ok().body(messageResponse);
+    }
+
+    private void logRequestData(HttpServletRequest request) {
+        logger.info("Request from: {}://{}:{}", request.getScheme(), request.getRemoteHost(), request.getRemotePort());
+        logger.info("To: {}://{}:{}{}", request.getScheme(), request.getServerName(), request.getServerPort(), request.getRequestURI());
     }
 }
--- src/main/java/com/serial30/diagnocom/controllers/HomeController.java
+++ src/main/java/com/serial30/diagnocom/controllers/HomeController.java
@@ -1,6 +1,9 @@
 package com.serial30.diagnocom.controllers;
 
 import com.serial30.diagnocom.pojos.response.MessageResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -8,16 +11,24 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin("*")
-@RequestMapping("chainlogic")
+@RequestMapping("${app.name}")
 public class HomeController {
+
+    private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 
     @Value("${app.name}")
     private String appName;
 
     @RequestMapping(method = RequestMethod.GET, path = "status", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> status() {
+    public ResponseEntity<?> status(HttpServletRequest request) {
+        logRequestData(request);
         var response = new MessageResponse();
         response.setData(appName.toUpperCase() + " microservice is active!");
         return ResponseEntity.status(response.getStatus()).body(response);
+    }
+
+    private void logRequestData(HttpServletRequest request) {
+        logger.info("Request from: {}://{}:{}", request.getScheme(), request.getRemoteHost(), request.getRemotePort());
+        logger.info("To: {}://{}:{}{}", request.getScheme(), request.getServerName(), request.getServerPort(), request.getRequestURI());
     }
 }
